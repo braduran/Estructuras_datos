88e808e8da6daeba9bff2e9668c3ebf50e5b4466
Brayan <Brayan@192.168.1.58>
2019-12-14 16:26:44 -0500

    Algoritmo dijkstra en matriz adyacente

--- src/main/java/com/grafos/matriz/Main.java
+++ src/main/java/com/grafos/matriz/Main.java
@@ -7,49 +7,49 @@ public class Main {
 	public static void main(String[] args) {
 		Matriz matriz = new Matriz(10);
 	    
-		matriz.agregar(0, 5);
-		matriz.agregar(0, 6);
-		matriz.agregar(0, 9);
+		matriz.agregar(0, 5, 8);
+		matriz.agregar(0, 6, 8);
+		matriz.agregar(0, 9, 2);
 		
-		matriz.agregar(1, 4);
-		matriz.agregar(1, 6);
+		matriz.agregar(1, 4, 4);
+		matriz.agregar(1, 6, 2);
 		
-		matriz.agregar(2, 5);
-		matriz.agregar(2, 7);
-		matriz.agregar(2, 8);
-		matriz.agregar(2, 9);
+		matriz.agregar(2, 5, 6);
+		matriz.agregar(2, 7, 4);
+		matriz.agregar(2, 8, 6);
+		matriz.agregar(2, 9, 4);
 		
-		matriz.agregar(3, 6);
-		matriz.agregar(3, 9);
+		matriz.agregar(3, 6, 8);
+		matriz.agregar(3, 9, 2);
 		
-		matriz.agregar(4, 1);
-		matriz.agregar(4, 7);
-		matriz.agregar(4, 8);
+		matriz.agregar(4, 1, 4);
+		matriz.agregar(4, 7, 2);
+		matriz.agregar(4, 8, 6);
 		
-		matriz.agregar(5, 0);
-		matriz.agregar(5, 2);
-		matriz.agregar(5, 9);
+		matriz.agregar(5, 0, 8);
+		matriz.agregar(5, 2, 6);
+		matriz.agregar(5, 9, 4);
 		
-		matriz.agregar(6, 0);
-		matriz.agregar(6, 1);
-		matriz.agregar(6, 3);
+		matriz.agregar(6, 0, 8);
+		matriz.agregar(6, 1, 2);
+		matriz.agregar(6, 3, 8);
 		
-		matriz.agregar(7, 2);
-		matriz.agregar(7, 4);
-		matriz.agregar(7, 9);
+		matriz.agregar(7, 2, 4);
+		matriz.agregar(7, 4, 2);
+		matriz.agregar(7, 9, 2);
 		
-		matriz.agregar(8, 2);
-		matriz.agregar(8, 4);
+		matriz.agregar(8, 2, 6);
+		matriz.agregar(8, 4, 6);
 		
-		matriz.agregar(9, 0);
-		matriz.agregar(9, 2);
-		matriz.agregar(9, 3);
-		matriz.agregar(9, 5);
-		matriz.agregar(9, 7);
+		matriz.agregar(9, 0, 2);
+		matriz.agregar(9, 2, 4);
+		matriz.agregar(9, 3, 2);
+		matriz.agregar(9, 5, 4);
+		matriz.agregar(9, 7, 2);
 	    
 	    matriz.imprimir();
-	    JOptionPane.showMessageDialog(null, matriz.gradoVertice(2));
-	    
-	    matriz.DFS_Madya(3);
+	    //JOptionPane.showMessageDialog(null, matriz.gradoVertice(2));
+	    //matriz.DFS_Madya(3);
+	    matriz.dijkstra(3);
 	}
 }
--- src/main/java/com/grafos/matriz/Matriz.java
+++ src/main/java/com/grafos/matriz/Matriz.java
@@ -11,8 +11,8 @@ public class Matriz {
 		visitado = new int[n];
 	}
 	
-	public void agregar(int i, int j){
-        matriz[i][j] += 1;
+	public void agregar(int i, int j, int distancia){
+        matriz[i][j] = distancia;
     }
 	
 	public void remover(int i, int j){
@@ -53,5 +53,66 @@ public class Matriz {
 				}
 			}
 		}
+	}
+	
+	public void dijkstra(int v) {
+		int cost[][] = new int[n][n];
+		int distance[] = new int[n];
+		int pred[]= new int[n];
+		int visited[] = new int[n];
+		int count, mindistance, nextnode = 0, i, j;
+		
+		   for(i=0;i<n;i++) {
+		      for(j=0;j<n;j++) {
+			   if(matriz[i][j]==0){
+			      cost[i][j] = Integer.MAX_VALUE;
+			   }else {
+			      cost[i][j] = matriz[i][j];
+			   }
+		      }
+		   }
+			   
+		   for(i=0; i<n; i++) {
+		      distance[i] = cost[v][i];
+		      pred[i] = v;
+		      visited[i] = 0;
+		   }
+		   
+		   distance[v]=0;
+		   visited[v]=1;
+		   count=1;
+		   
+		   while(count<n-1) {
+		      mindistance= Integer.MAX_VALUE;
+		      
+		      for(i=0; i<n; i++) {
+		         if(distance[i] < mindistance && visited[i] == 0) {
+			         mindistance = distance[i];
+			         nextnode = i;
+		         }
+		      }
+		      
+		      visited[nextnode]=1;
+		      
+		      for(i=0;i<n;i++) {
+		         if(visited[i] == 0) {
+					  if(mindistance+cost[nextnode][i]<distance[i]) {
+					     distance[i]=mindistance+cost[nextnode][i];
+					     pred[i]=nextnode;
+					  }
+		         }
+			      count++;
+		      }
+		   }
+		   
+		   for(i=0; i<n; i++)
+		   if(i != v) {
+			   System.out.println("Distancia del nodo " + i + " = " + distance[i] + " desde " + i);
+			   j = 1;
+			   do {
+		         j=pred[j];
+		         System.out.println(" <- " + j);
+			   }while(j!=v);
+		   }
 	}
 }
